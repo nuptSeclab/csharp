f40ab7a1cc8ad62232a50cb5b63425c1d89a4f83
imwack <i.am.wack.1993@gmail.com>
2016-03-18 20:09:54 +0800

    实现页面跳转。输入密码

--- WindowsFormsApplication/MainForm.cs
+++ WindowsFormsApplication/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,18 @@ namespace WindowsFormsApplication
 {
     public partial class MainForm : Form
     {
+        [DllImport("user32.dll", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]
+        private extern static IntPtr FindWindow(string classname, string captionName);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", CharSet = CharSet.Auto)]
+        private extern static IntPtr FindWindowEx(IntPtr parent, IntPtr child, string classname, string captionName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, [MarshalAs(UnmanagedType.LPStr)] string lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
         public MainForm()
         {
             InitializeComponent();
@@ -49,23 +62,23 @@ namespace WindowsFormsApplication
                 {
                     //MessageBox.Show("find!");
                     elem.InvokeMember("click"); //点击登陆
-
-                    Thread.Sleep(3000);
+                    this.timer1.Enabled = true;
+                    //Thread.Sleep(3000);
                     //模拟输入 密码
-                    IntPtr hwndCalc = FindWindow("#32770", null); //查找窗口句柄
-                    if (hwndCalc != IntPtr.Zero)//找到
-                    {
-                        SetForegroundWindow(hwndCalc);
-                        System.Windows.Forms.SendKeys.SendWait("{TAB}");
-                        System.Windows.Forms.SendKeys.SendWait("{TAB}");
-                        System.Windows.Forms.SendKeys.SendWait("123456");
-                        System.Windows.Forms.SendKeys.SendWait("{TAB}");
-                        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                    }
-                    else
-                    {
-                       // MessageBox.Show("没有启动 ");
-                    }
+                    //IntPtr hwndCalc = FindWindow("#32770", null); //查找窗口句柄
+                    //if (hwndCalc != IntPtr.Zero)//找到
+                    //{
+                    //    SetForegroundWindow(hwndCalc);
+                    //    System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                    //    System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                    //    System.Windows.Forms.SendKeys.SendWait("123456");
+                    //    System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                    //    System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                    //}
+                    //else
+                    //{
+                    //   // MessageBox.Show("没有启动 ");
+                    //}
                 }         
 
             }
@@ -84,5 +97,23 @@ namespace WindowsFormsApplication
             //}
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IntPtr mwh1 = IntPtr.Zero;
+            mwh1 = FindWindow(null, "数字证书登录");
+            if (mwh1 != IntPtr.Zero)
+            {
+                IntPtr edit = FindWindowEx(mwh1, IntPtr.Zero, "Edit", null);
+              //  IntPtr button = FindWindowEx(mwh1, IntPtr.Zero, "Button", "确定");
+                if (edit != IntPtr.Zero)
+                {
+                    //MessageBox.Show("find edit");
+                    SendMessage(edit, 0xC, IntPtr.Zero, "123456");
+                    SendKeys.SendWait("{Enter}");
+                    SendKeys.Flush();
+                    this.timer1.Dispose();    //关闭定时器
+                }
+            }
+        }
     }
 }
