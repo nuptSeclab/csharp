89cbcadbaa73b50a2f978c0944109548a2550a81
imwack <i.am.wack.1993@gmail.com>
2016-03-18 16:40:46 +0800

    模拟按键实现，但是网页跳转会出现证书错误

--- WindowsFormsApplication/Form1.Designer.cs
+++ WindowsFormsApplication/Form1.Designer.cs
@@ -28,29 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.x = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 49);
+            this.textBox1.Location = new System.Drawing.Point(44, 35);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(77, 103);
+            this.textBox2.Location = new System.Drawing.Point(44, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(95, 177);
+            this.textBox3.Location = new System.Drawing.Point(44, 126);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 21);
             this.textBox3.TabIndex = 2;
@@ -65,11 +69,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(193, 126);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(62, 21);
+            this.x.TabIndex = 4;
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(193, 165);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(62, 21);
+            this.y.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -87,5 +113,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox y;
     }
 }
--- WindowsFormsApplication/Form1.cs
+++ WindowsFormsApplication/Form1.cs
@@ -14,17 +14,20 @@ namespace WindowsFormsApplication
 
     public partial class Form1 : Form
     {
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);//查找窗口句柄
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, uint hwndChildAfter, string lpszClass, string lpszWindow);//查找窗口内控件句柄
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hwnd, uint wMsg, int wParam, int lParam);//发送消息
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow", SetLastError = true)]
+        private static extern void SetForegroundWindow(IntPtr hwnd);// 设置窗口为激活状态
         [DllImport("user32", SetLastError = true)]
         public static extern int GetWindowText(
-     IntPtr hWnd,//窗口句柄 
-     StringBuilder lpString,//标题 
-     int nMaxCount //最大值 
-     );
+            IntPtr hWnd,//窗口句柄 
+            StringBuilder lpString,//标题 
+            int nMaxCount //最大值 
+            );
         //获取类的名字 
         [DllImport("user32.dll")]
         private static extern int GetClassName(
@@ -39,11 +42,50 @@ namespace WindowsFormsApplication
         );
 
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+
         private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr hwndCalc = FindWindow("#32770",null); //查找窗口句柄
+            if (hwndCalc != IntPtr.Zero)//找到
+            {
+    
+                
+                SetForegroundWindow(hwndCalc);
+                System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                System.Windows.Forms.SendKeys.SendWait("123456");
+                System.Windows.Forms.SendKeys.SendWait("{TAB}");
+                System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                //const uint WM_SETTEXT = 0x000C;//设置文本框内容的消息
+                //const uint BM_CLICK = 0xF5; //鼠标点击的消息，对于各种消息的数值，你还是得去查查API手册
+                //IntPtr hwndLogin = FindWindowEx(hwndCalc, 0, null, "确定"); //获取登陆按钮的句柄
+                //IntPtr hwndP = FindWindowEx(hwndCalc, 0, "", "Edit");  //获取密码输入框的控件句柄 SetForegroundWindow ( hwndCalc );    //将QQ窗口设置为激活
+
+                //System.Threading.Thread.Sleep(1000);   //暂停1秒让你看到效果
+                //SendMessage(hwndP, WM_SETTEXT, 0, 0);//发送文本框2里面的内容（QQpassword）
+
+                //System.Threading.Thread.Sleep(1000);   //暂停1秒让你看到效果
+                //SendMessage(hwndLogin, BM_CLICK, 0, 0);//点击登录
+            }
+            else
+            {
+                MessageBox.Show("没有启动 ");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
             int x = Cursor.Position.X;
             int y = Cursor.Position.Y;
             Point p = new Point(x, y);
+            this.x.Text = x.ToString();
+            this.y.Text = y.ToString();
             IntPtr formHandle = WindowFromPoint(p);//得到窗口句柄 
             StringBuilder title = new StringBuilder(256);
             GetWindowText(formHandle, title, title.Capacity);//得到窗口的标题 
@@ -52,6 +94,7 @@ namespace WindowsFormsApplication
             this.textBox1.Text = title.ToString();
             this.textBox2.Text = formHandle.ToString();
             this.textBox3.Text = className.ToString();
+
         }
     }
    }
